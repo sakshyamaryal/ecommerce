3bd1767ff1bb46cc20b81b78efea7bebb3f2e31c
sakshyamaryal <sakshyamaryal1@gmail.com>
2024-03-07 16:48:14 +0545

    order modal not working

--- ecommerce/resources/views/layouts/app.blade.php
+++ ecommerce/resources/views/layouts/app.blade.php
@@ -5,15 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel ecommerce')</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-pzjw8f+XtGLeJuZYyWzstwASK+v0hMHvhDLX+va5bXCEhFv/Z6a8r0+s2O6ste7I" crossorigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script> <!-- Include SweetAlert library -->
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="app">
--- ecommerce/resources/views/products/index.blade.php
+++ ecommerce/resources/views/products/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
@@ -49,6 +50,9 @@
                                         <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary">
                                             <i class="fa fa-edit"></i> Edit
                                         </a>
+                                        <button class="btn btn-info view-orders" data-product-id="{{ $product->id }}">
+                                            <i class="fa fa-eye"></i> View Orders
+                                        </button>
                                         <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display: inline-block;">
                                             @csrf
                                             @method('DELETE')
@@ -67,4 +71,56 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="ordersModal" tabindex="-1" role="dialog" aria-labelledby="ordersModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ordersModalLabel">Orders for Product</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" id="ordersModalBody">
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function () {
+        $('.view-orders').on('click', function (e) {
+            e.preventDefault();
+            var productId = $(this).data('product-id');
+            $.ajax({
+                url: '/products/' + productId + '/orders',
+                type: 'GET',
+                success: function (response) {
+                    // Populate modal with orders data
+                    // Assuming you have a modal with id 'ordersModal' and a body with id 'ordersModalBody'
+                    $('#ordersModalBody').empty();
+                    if (response.length > 0) {
+                        var ordersHtml = '';
+                        $.each(response, function (index, order) {
+                            ordersHtml += '<p>Order ID: ' + order.id + '</p>';
+                            // Add more order details as needed
+                        });
+                        $('#ordersModalBody').html(ordersHtml);
+                    } else {
+                        $('#ordersModalBody').html('<p>No orders found for this product.</p>');
+                    }
+                    $('#ordersModal').modal('show');
+                },
+                error: function (xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
+        });
+    });
+</script>
+
 @endsection
